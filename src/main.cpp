b70a3b8c96c2aa4551b537eec86fef0a221d27ea
Dino Tinitigan <bigdinotech@gmail.com>
2016-06-22 11:27:58 -0700

    cdc-acm Tx
    -enables cdc-acm tx

--- src/main.cpp
+++ src/main.cpp
@@ -47,8 +47,105 @@
 #define RSTC_COLD_RESET (1 << 3)
 
 #define CDC_ACM_DEVICE "CDC_ACM"
+
+static volatile bool data_transmitted;
+static volatile bool data_arrived;
+static unsigned char data_buf[64];
+
 struct device *dev;
 bool usbSetupDone = false;
+
+/**
+ * Use the following defines just to make the tips of your finger happier.
+ */
+/**
+#define Rx_BUFF cdc_acm_shared_rx_buffer.data
+#define Rx_HEAD cdc_acm_shared_rx_buffer.head
+#define Rx_TAIL cdc_acm_shared_rx_buffer.tail
+#define Tx_BUFF cdc_acm_shared_tx_buffer.data
+#define Tx_HEAD cdc_acm_shared_tx_buffer.head
+#define Tx_TAIL cdc_acm_shared_tx_buffer.tail
+**/
+#define Rx_BUFF shared_data->cdc_acm_shared_rx_buffer.data
+#define Rx_HEAD shared_data->cdc_acm_shared_rx_buffer.head
+#define Rx_TAIL shared_data->cdc_acm_shared_rx_buffer.tail
+#define Tx_BUFF shared_data->cdc_acm_shared_tx_buffer.data
+#define Tx_HEAD shared_data->cdc_acm_shared_tx_buffer.head
+#define Tx_TAIL shared_data->cdc_acm_shared_tx_buffer.tail
+#define SBS     SERIAL_BUFFER_SIZE
+
+/* Make sure BUFFER_LENGTH is not bigger then shared ring buffers */
+#define BUFFER_LENGTH		128
+
+#define LOOP_INTERVAL_MS 1
+#define USB_ACM_TIMEOUT_MS (250)
+
+#define USB_CONNECTED	    0x04
+#define USB_DISCONNECTED    0x05
+
+static struct cdc_ring_buffer* cdc_acm_shared_rx_buffer;
+static struct cdc_ring_buffer* cdc_acm_shared_tx_buffer;
+//volatile struct cdc_acm_shared_data cdc_acm_buffers;
+
+static uint8_t read_buffer[BUFFER_LENGTH*2];
+static uint8_t write_buffer[BUFFER_LENGTH*2];
+
+typedef enum {
+	ACM_RX_DISABLED,
+	ACM_RX_READY,
+	ACM_RX_PENDING
+} acm_rx_states;
+
+typedef enum {
+	ACM_TX_DISABLED,
+	ACM_TX_READY,
+	ACM_TX_PENDING
+} acm_tx_states;
+
+static volatile uint32_t acm_rx_state = ACM_RX_DISABLED;
+static volatile uint32_t acm_tx_state = ACM_TX_DISABLED;
+
+void cdc_acm_tx();
+
+
+static void interrupt_handler(struct device *dev)
+{
+	uart_irq_update(dev);
+
+	if (uart_irq_tx_ready(dev)) {
+		data_transmitted = true;
+	}
+
+	if (uart_irq_rx_ready(dev)) {
+		data_arrived = true;
+	}
+}
+
+static void read_data(struct device *dev, int *bytes_read)
+{
+	uart_irq_rx_enable(dev);
+
+	data_arrived = false;
+	while (data_arrived == false)
+		;
+
+	/* Read all data */
+	*bytes_read = uart_fifo_read(dev, data_buf, sizeof(data_buf));
+
+	uart_irq_rx_disable(dev);
+}
+
+static void write_data(struct device *dev, const char *buf, int len)
+{
+	uart_irq_tx_enable(dev);
+
+	data_transmitted = false;
+	uart_fifo_fill(dev, (const uint8_t*)buf, len);
+	while (data_transmitted == false)
+		;
+
+	uart_irq_tx_disable(dev);
+}
 
 void start_arc(unsigned int reset_vector)
 {
@@ -79,12 +176,58 @@ void reboot(void)
 
 void main(void)
 {
+	shared_data->cdc_acm_buffers.rx_buffer = &shared_data->cdc_acm_shared_rx_buffer;
+	shared_data->cdc_acm_buffers.tx_buffer = &shared_data->cdc_acm_shared_tx_buffer;
+
 	//start ARC core
 	uint32_t *reset_vector;
 	reset_vector = (uint32_t *)RESET_VECTOR;
 	start_arc(*reset_vector);
+
+	uint32_t baudrate, dtr = 0;
+	int ret;
+
+
 	dev = device_get_binding(CDC_ACM_DEVICE);
+
+	PRINT("Wait for DTR\n");
+	while (1) {
+		uart_line_ctrl_get(dev, LINE_CTRL_DTR, &dtr);
+		if (dtr)
+			break;
+	}
+	
+	PRINT("DTR set, start test\n");
+
+	/* They are optional, we use them to test the interrupt endpoint */
+	ret = uart_line_ctrl_set(dev, LINE_CTRL_DCD, 1);
+	if (ret)
+		PRINT("Failed to set DCD, ret code %d\n", ret);
+	
+	acm_tx_state = ACM_TX_READY;
+	shared_data->cdc_acm_buffers.host_open = true;
+	
+	ret = uart_line_ctrl_set(dev, LINE_CTRL_DSR, 1);
+	if (ret)
+		PRINT("Failed to set DSR, ret code %d\n", ret);
+
+	/* Wait 1 sec for the host to do all settings */
+	sys_thread_busy_wait(1000000);
+
+	ret = uart_line_ctrl_get(dev, LINE_CTRL_BAUD_RATE, &baudrate);
+	if (ret)
+		PRINT("Failed to get baudrate, ret code %d\n", ret);
+	else
+		PRINT("Baudrate detected: %d\n", baudrate);
+
+	uart_irq_callback_set(dev, interrupt_handler);
 	usbSetupDone = true;
+	
+	//reset head and tails values to 0
+	shared_data->cdc_acm_shared_rx_buffer.head = 0;
+	shared_data->cdc_acm_shared_rx_buffer.tail = 0;
+	shared_data->cdc_acm_shared_tx_buffer.head = 0;
+	shared_data->cdc_acm_shared_tx_buffer.tail = 0;
 }
 
 extern "C" void baudrateReset(void)
@@ -104,3 +247,63 @@ extern "C" void baudrateReset(void)
 		task_sleep(100);
 	}
 }
+
+extern "C" void usbSerialTask(void)
+{
+	int32_t bytes_read = 0;
+
+	while(!usbSetupDone);
+	//write_data(dev, banner1, strlen(banner1));
+	//write_data(dev, banner2, strlen(banner2));
+
+	
+	/* Echo the received data */
+	PRINT("characters read: \n");
+	uint32_t baudrate, ret = 0;
+	ret = uart_line_ctrl_get(dev, LINE_CTRL_BAUD_RATE, &baudrate);
+	static const char *baud = "baudrate 1200. Reset board\r\n";
+
+	
+	while (1) {
+		cdc_acm_tx();
+		task_sleep(10);
+		ret = uart_line_ctrl_get(dev, LINE_CTRL_BAUD_RATE, &baudrate);
+		if(baudrate == 1200)
+		{
+			write_data(dev, baud, strlen(baud));
+			reboot();
+		}
+	}
+	
+}
+
+void cdc_acm_tx()
+{
+	//int head = shared_data->cdc_acm_shared_tx_buffer.head;
+	//int tail = shared_data->cdc_acm_shared_tx_buffer.tail;
+
+	if (acm_tx_state == ACM_TX_READY) 
+	{
+		if(Tx_HEAD != Tx_TAIL)
+		{
+			int cnt = 0, index = Tx_TAIL;
+			for (; (index != Tx_HEAD) && (cnt < BUFFER_LENGTH);cnt++) 
+			{
+				write_buffer[cnt] = Tx_BUFF[index];
+				index = (index + 1) % SBS;
+			}
+			Tx_TAIL= (Tx_TAIL + cnt) % SBS;
+			write_data(dev, (const char*)write_buffer, cnt);
+		}
+		else
+		{
+
+		}
+	}
+	else if (acm_tx_state == ACM_TX_DISABLED) 
+	{
+		Tx_TAIL = Tx_HEAD;
+	}
+}
+
+ 
