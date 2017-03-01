fadde366e07840b22559245c26d1ed7cdb3a045c
Dino Tinitigan <bigdinotech@gmail.com>
2017-03-01 12:43:53 -0800

    Read USB Device Descriptors from OTP

--- src/main.c
+++ src/main.c
@@ -23,6 +23,9 @@
 #include "soc_ctrl.h"
 #include "cdcacm_serial.h"
 #include "curie_shared_mem.h"
+#include <string.h>
+#include <cdc_acm_config.h>
+#include <factory_data.h>
 
 #define SOFTRESET_INTERRUPT_PIN		0
 
@@ -36,9 +39,18 @@
 #define PRIORITY 7
 #define TASK_PRIORITY 8
 
+#define ARDUINO_101_VID		0x8087
+#define ARDUINO_101_PID		0x0AB6
+
+#define MAX_DESCRIPTOR_LENGTH	64
+
 char __noinit __stack cdcacm_setup_stack_area[CDCACM_STACKSIZE];
 char __noinit __stack baudrate_reset_stack_area[RESET_STACKSIZE];
 char __noinit __stack usb_serial_stack_area[USBSERIAL_STACKSIZE];
+
+const char *vendor = "Intel";
+const char *product =  "Arduino 101";
+const char *serial = "00.01";
 
 struct gpio_callback cb;
 
@@ -47,6 +59,48 @@ void softResetButton();
 static void softReset_button_callback(struct device *port, struct gpio_callback *cb, uint32_t pins)
 {
 	soft_reboot();
+}
+
+void copy_device_descriptor(char *dest, uint8_t *source, char *default_value, uint8_t len, uint8_t max)
+{
+	if ((len > 0) && (len < max))
+	{
+		memcpy(dest, source, len);
+		dest[len] = '\0';
+	}
+	else
+	{
+		memcpy(dest, default_value, strlen(default_value));
+		dest[strlen(default_value)] = '\0';
+	}
+}
+
+void cdc_acm_descriptor_callback (cdc_acm_cfg_t *cfg)
+{
+	customer_data_t *otp_data = (customer_data_t *)CUSTOMER_DATA_ADDRESS;
+
+	//read the USB descriptors from OTP and use some default values if it has not been properly populated
+	copy_device_descriptor(cfg->vendor_string, otp_data->vendor_name, vendor,
+		otp_data->vendor_name_len, MAX_DESCRIPTOR_LENGTH);
+
+	copy_device_descriptor(cfg->product_string, otp_data->board_name, product,
+		otp_data->board_name_len, MAX_DESCRIPTOR_LENGTH);
+
+	copy_device_descriptor(cfg->serial_string, otp_data->product_sn, serial,
+		otp_data->product_sn_len, MAX_DESCRIPTOR_LENGTH);
+
+	if((otp_data->product_vid != 0xFFFF) && (otp_data->product_vid != 0x0000)
+		&& (otp_data->product_pid != 0xFFFF) && (otp_data->product_pid != 0x0000))
+	{
+		cfg->vendor_id = otp_data->product_vid;
+		cfg->product_id = otp_data->product_pid;
+	}
+	else
+	{
+		//use VID/PID of Arduino 101
+		cfg->vendor_id = ARDUINO_101_VID;
+		cfg->product_id = ARDUINO_101_PID;
+	}
 }
 
 void threadMain(void *dummy1, void *dummy2, void *dummy3)
